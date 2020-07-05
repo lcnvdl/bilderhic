46d9be81acef8faed16923a6699abe7df22dd3d4
Lcnvdl <programando.soft@gmail.com>
2020-07-05 15:25:02 -0300

    Ignore argument added to copy command

--- src/commands/copy/index.js
+++ src/commands/copy/index.js
@@ -7,14 +7,37 @@ class CopyCommand extends CommandBase {
             return this.codes.missingArguments;
         }
 
-        if (args.length > 2) {
+        if (args.length < 2) {
             return this.codes.invalidArguments;
         }
 
         let file1 = this.parsePath(args[0]);
         let file2 = this.parsePath(args[1]);
+        let ignores = [];
 
-        fse.copySync(file1, file2);
+        for (let i = 2; i < args.length; i++) {
+            const arg = args[i];
+            if (arg === "-i" || arg === "--ignore") {
+                ignores.push(args[++i]);
+            }
+            else {
+                return this.codes.invalidArguments;
+            }
+        }
+
+        fse.copySync(file1, file2, {
+            filter: (src, dest) => {
+                if (ignores.length > 0) {
+                    for (let ignore of ignores) {
+                        if (src === ignore || src.endsWith("/" + ignore) || src.endsWith("\\" + ignore)) {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+        });
 
         return this.codes.success;
     }
