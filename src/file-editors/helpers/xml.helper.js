02a80f03e6cf3ef2eb0f953e9fbd65e161c964c9
Lcnvdl <programando.soft@gmail.com>
2020-07-06 12:33:38 -0300

    Fix

--- src/file-editors/helpers/xml.helper.js
+++ src/file-editors/helpers/xml.helper.js
@@ -4,14 +4,14 @@ class XmlHelper {
      * @param {string[]} [ignoreTags] Ignore tags
      */
     static generateSelfClosingTags(content, ignoreTags) {
-        const regexp = /([>](\s*[<][/]\s*(\w|[-])+\s*[>]))/gi;
+        const regexp = /([>](\s*[<][/]\s*((\w|[-])+)\s*[>]))/gi;
         let result;
         let finalContent = content;
 
         while ((result = regexp.exec(content)) !== null) {
             const all = result[1];
             const tag = result[3];
-            
+
             if (!ignoreTags.includes(tag)) {
                 finalContent = finalContent.replace(all, "/>");
             }
--- test/file-editors.helpers.xml-helper.test.js
+++ test/file-editors.helpers.xml-helper.test.js
@@ -3,14 +3,39 @@ const { expect } = require("chai");
 
 describe("XmlHelper", () => {
     describe("normalizeEmptyTags", () => {
-        it("should work fine", () => {
+        it("ignore tag", () => {
             const content = "<i></i><br></br>";
             const expected = "<i></i><br/>";
             const result = XmlHelper.generateSelfClosingTags(content, ["i"]);
 
             expect(result).to.equal(expected);
         });
-        it("should work fine", () => {
+
+        it("nested tags", () => {
+            const content = "<i><br></br></i>";
+            const expected = "<i><br/></i>";
+            const result = XmlHelper.generateSelfClosingTags(content, ["i"]);
+
+            expect(result).to.equal(expected);
+        });
+
+        it("nested with ignore", () => {
+            const content =
+                '        <plugin name="cordova-plugin-googleplus" spec="8.5.0">\n' +
+                '            <variable name="REVERSED_CLIENT_ID" value="com.ionic.app"></variable>\n' +
+                '            <variable name="WEB_APPLICATION_CLIENT_ID" value="123123123-aaaaaaaaaaaaaaajupgo.apps.googleusercontent.com"></variable>\n' +
+                '        </plugin>\n';
+            const expected = 
+            '        <plugin name="cordova-plugin-googleplus" spec="8.5.0">\n' +
+            '            <variable name="REVERSED_CLIENT_ID" value="com.ionic.app"></variable>\n' +
+            '            <variable name="WEB_APPLICATION_CLIENT_ID" value="123123123-aaaaaaaaaaaaaaajupgo.apps.googleusercontent.com"></variable>\n' +
+            '        </plugin>\n';
+            const result = XmlHelper.generateSelfClosingTags(content, ["variable"]);
+
+            expect(result).to.equal(expected);
+        });
+
+        it("single tag with attributes", () => {
             const content = "<my-property id='123'></my-property>";
             const expected = "<my-property id='123'/>";
             const result = XmlHelper.generateSelfClosingTags(content, ["i"]);
