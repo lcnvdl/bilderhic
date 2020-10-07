41dc3d37efa78e3cb923b0c51d2f8a94d8f92a17
Lcnvdl <programando.soft@gmail.com>
2020-10-07 13:27:32 -0300

    Update index.js

--- src/commands/extension/index.js
+++ src/commands/extension/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const CommandBase = require("../base/command-base");
 const inquirer = require('inquirer');
 const DirectoryHelper = require("../helpers/directory-helper");
+const RunCommand = require("../run/index");
 
 class ExtensionCommand extends CommandBase {
     async run(args) {
@@ -10,8 +11,21 @@ class ExtensionCommand extends CommandBase {
                 return this.codes.missingArguments;
             }
 
-            if (args[1].startsWith("git:")) {
-                //  TODO    Git clone
+            const dir = new DirectoryHelper();
+
+            if (args[1].startsWith("git:") || args[1].endsWith(".git")) {
+                this.info(`Installing ${args[1]}...`);
+
+                this.breakpoint();
+
+                const env = this.environment.fork(dir.commandsDir);
+                const command = new RunCommand(env);
+                await command.run(["git", "clone", args[1]]);
+
+                this.info(`Package installed`);
+            }
+            else {
+                this.codes.invalidArguments;
             }
 
             return this.codes.success;
